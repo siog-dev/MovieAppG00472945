f3966d54757cefbf6e9f12afd1fbb8b4a7030413
Siog Murphy <git@siog.dev>
2026-05-06 13:11:17 +0100

    Add a get method for the http service

--- src/app/services/my-http.ts
+++ src/app/services/my-http.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -7,5 +8,9 @@ import { HttpClient } from '@angular/common/http';
 export class MyHttp {
 
   constructor(private http: HttpClient) { }
+
+  get(): Observable<any> {
+    return this.http.get("https://api.themoviedb.org/3/trending/movie/day?api_key=862da29609cec096571a286070ebb32d")
+  }
   
 }
