9fd3208bf836d9488d1bf733556fde43b302c89b
Siog Murphy <git@siog.dev>
2026-05-06 15:32:12 +0100

    Display trending movies on homepage from TMDB API

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -5,15 +5,17 @@ import { heart } from 'ionicons/icons'; // name the icons
 import { RouterLink } from '@angular/router'; // manually import RouterLink (lecture notes, create an ionic app)
 import { FormsModule } from '@angular/forms'; // for ngModel use
 import { MyHttp } from '../services/my-http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, RouterLink, IonInput, IonButtons, FormsModule],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, RouterLink, IonInput, IonButtons, FormsModule, CommonModule],
 })
 export class HomePage {
 
-  searchTerm = '';
+  searchTerm = "";
+  movies:any = [];
 
   constructor(private mhs: MyHttp) {
     addIcons({heart}); // register the icons for use everywhere
@@ -26,9 +28,12 @@ export class HomePage {
   getTrending() {
     this.mhs.get("https://api.themoviedb.org/3/trending/movie/day?api_key=862da29609cec096571a286070ebb32d").subscribe(
       {
-        next: (results) => console.log(results),
+        next: (data) => {
+          console.log(data);
+          this.movies = data.results;
+        },
         error: (error) => console.error("error", error),
-        complete: () => console.log('complete')
+        complete: () => console.log("complete")
     })
   }
 
