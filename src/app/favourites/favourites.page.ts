edb1ac25a84374a69666d153efa056848036e9e5
Siog Murphy <git@siog.dev>
2026-05-07 00:53:28 +0100

    Load favourites to favourites page

--- src/app/favourites/favourites.page.ts
+++ src/app/favourites/favourites.page.ts
@@ -1,19 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
+import { addIcons } from 'ionicons'; // add specific icons
+import { heart, homeOutline, heartOutline } from 'ionicons/icons'; // name of the icons
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon, IonCard, IonCardContent } from '@ionic/angular/standalone';
+import { MyFavourites } from '../services/my-favourites';
 
 @Component({
   selector: 'app-favourites',
   templateUrl: './favourites.page.html',
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonButton, IonIcon, RouterLink, IonCard, IonCardContent]
 })
 export class FavouritesPage implements OnInit {
 
-  constructor() { }
+  favourites:any = [];
+
+  constructor(private mfs: MyFavourites) { 
+    addIcons({heart, homeOutline, heartOutline});
+  }
 
   ngOnInit() {
+    this.loadFavourites();
+  }
+
+  async loadFavourites() {
+    this.favourites = await this.mfs.getFavourites();
+    console.log(this.favourites);
   }
 
 }
