b2099e6c4600e206f253ca713aaf55444745a6cf
Siog Murphy <git@siog.dev>
2026-05-06 00:13:17 +0000

    Connect the search bar and the search button
    Use the ngModel to bind the input value to searchTerm, use a click event to call the onSearch() method which in turn prints the input value to the console

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,17 +1,25 @@
 import { Component } from '@angular/core';
-// import IonIcon
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonLabel, IonIcon, IonInput, IonButtons } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, IonInput, IonButtons } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons'; // add specific icons
 import { heart } from 'ionicons/icons'; // name the icons
 import { RouterLink } from '@angular/router'; // manually import RouterLink (lecture notes, create an ionic app)
+import { FormsModule } from '@angular/forms'; // for ngModel use
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonItem, IonLabel, IonIcon, RouterLink, IonInput, IonButtons],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, IonIcon, RouterLink, IonInput, IonButtons, FormsModule],
 })
 export class HomePage {
+
+  searchTerm = '';
+
   constructor() {
     addIcons({heart}); // register the icons for use everywhere
   }
+
+  onSearch() {
+    console.log(this.searchTerm); // print the input to console when search is pressed
+  }
+
 }
